f2988f4f806c4713f53b1b55731dd9056fc512c7
erzhena <erzhenadulzynova@gmail.com>
2026-08-18 07:22:39 +0100

    init

---
+++ src/main/java/com/erzhena/spaceexplorer_api/SpaceexplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.erzhena.spaceexplorer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceexplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceexplorerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erzhena/spaceexplorer_api/SpaceexplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erzhena.spaceexplorer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceexplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
