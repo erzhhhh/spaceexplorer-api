0c2efd454975cd062c4e7e4deb51f6266fc42b0a
erzhena <erzhenadulzynova@gmail.com>
2026-08-20 13:39:10 +0100

    add JpaRepository

---
+++ src/main/java/com/erzhena/spaceexplorer_api/ArticleRepository.java
@@ -0,0 +1,6 @@
+package com.erzhena.spaceexplorer_api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
